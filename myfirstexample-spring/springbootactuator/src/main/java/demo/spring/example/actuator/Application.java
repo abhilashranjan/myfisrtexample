ccb4d29114c13bd98b04dd93f0724296c9fc2bc3
abhilashranjan <ranjan.abhilash@gmail.com>
2017-03-07 23:08:43 +0530

    Added Spring boot example

---
+++ myfirstexample-spring/springbootactuator/src/main/java/demo/spring/example/actuator/Application.java
@@ -0,0 +1,13 @@
+package demo.spring.example.actuator;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by anya on 7/3/17.
+ */
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ myfirstexample-spring/springbootactuator/src/test/java/demo/spring/example/actuator/ApplicationTest.java
@@ -0,0 +1,22 @@
+package demo.spring.example.actuator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by anya on 7/3/17.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
