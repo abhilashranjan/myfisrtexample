50609492a14e21ca4408951a692c2dc6c0fdfa36
LAPTOP-MSSCUJEA\abhil <ranjan.abhilash@gmail.com>
2018-02-08 22:45:33 +0530

    Swap two node in list.

--- myfirstexample-algo/src/main/java/demo/myfirst/example/linklist/MergeList.java
+++ myfirstexample-algo/src/main/java/demo/myfirst/example/linklist/MergeList.java
@@ -84,6 +84,51 @@ public class MergeList {
         return head.next;
     }
 
+
+
+    public static void swapNodes(Node head,int x, int y)
+    {
+        // Nothing to do if x and y are same
+        if (x == y) return;
+
+        // Search for x (keep track of prevX and CurrX)
+        Node prevX = null, currX = head;
+        while (currX != null && currX.data != x)
+        {
+            prevX = currX;
+            currX = currX.next;
+        }
+
+        // Search for y (keep track of prevY and currY)
+        Node prevY = null, currY = head;
+        while (currY != null && currY.data != y)
+        {
+            prevY = currY;
+            currY = currY.next;
+        }
+
+        // If either x or y is not present, nothing to do
+        if (currX == null || currY == null)
+            return;
+
+        // If x is not head of linked list
+        if (prevX != null)
+            prevX.next = currY;
+        else //make y the new head
+            head = currY;
+
+        // If y is not head of linked list
+        if (prevY != null)
+            prevY.next = currX;
+        else // make x the new head
+            head = currX;
+
+        // Swap next pointers
+        Node temp = currX.next;
+        currX.next = currY.next;
+        currY.next = temp;
+    }
+
     public static void main(String[] args) {
         Node n1= new Node(1);
         Node n2= new Node(2);
@@ -94,11 +139,19 @@ public class MergeList {
         n1.next=n3;
         n3.next=n5;
 
+        n5.next=n2;
+
         n2.next=n4;
         n4.next=n6;
 
-        print(mergeTwoLists(n1,n2));
+//        print(mergeTwoLists(n1,n2));
+        print(n1);
         System.out.println("");
         //print(mergeLists(n1,n2));
+        swapNodes(n1,1,4);
+        print(n1);
+
     }
+
+
 }
